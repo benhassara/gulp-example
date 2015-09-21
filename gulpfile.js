1e6f5604b4cab21ff4f1fe4929368e9cddaf475f
benhassara <bhassara@gmail.com>
2015-09-21 13:44:58 -0600

    complete exercise

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var connect = require('gulp-connect');
+
+// configure connect task
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('*.html')
+    .pipe(connect.reload());
+});
 
 // configure jshint task
 gulp.task('jshint', function() {
-  return gulp.src('js/*.js')
+  return gulp.src('js/*.js') // update path!
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('js/*.js', ['jshint']);
+  gulp.watch('js/*.js', ['jshint']); // update path!
+  gulp.watch(['*.html'], ['html']);
 });
 
-gulp.task('default', ['watch']);
+// default task!
+gulp.task('default', ['watch', 'connect']);
